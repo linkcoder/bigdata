56a0fb0812b6cff80f47e2c2938c14dd29a32158
lincoder <718681050@qq.com>
2018-08-08 17:30:01 +0800

    wordcount（未分区）

---
+++ sparkOperate/src/main/java/cn/fg/scala/WordCount.scala
@@ -0,0 +1,28 @@
+package cn.fg.scala
+
+import org.apache.hadoop.yarn.util.ApplicationClassLoader
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+
+    conf.setAppName("wordCount")
+    conf.setMaster("local")
+
+    val sc=new SparkContext(conf)
+    val path="D:\\javapro\\bigdata\\sparkOperate\\src\\main\\resources\\data\\test1"
+    val rdd1=sc.textFile(path)
+
+    val rdd2=rdd1.flatMap(line => line.split(" "))
+
+    val rdd3=rdd2.map((_ ,1))
+
+    val rdd4=rdd3.reduceByKey(_ + _);
+
+    val rdd5= rdd4.collect();
+
+    rdd5.foreach(println)
+  }
+}
